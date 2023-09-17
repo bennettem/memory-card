8ade2abc31ee91d940964f308cecde8143a4a66f
bennettem <emily.pamelabennett@gmail.com>
2023-09-16 23:39:10 -0500

    fixed id issues

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 const unflipped = document.querySelectorAll(".unflipped");
-const flipped = document.querySelector(".flipped");
-const matchImgs = document.getElementById("match-img");
+const matchImgs = document.querySelectorAll(".match-img");
 const cards = document.querySelectorAll(".card");
 const cardContainer = document.querySelector(".card-container");
 
-//controls flipped/unflipped cards
 cards.forEach(card => {
   card.addEventListener("click", () => {
     card.classList.toggle("unflipped");
