a3faf6ac4a1b823f423982fcc2b2d538c86c669d
bennettem <emily.pamelabennett@gmail.com>
2023-09-17 01:01:13 -0500

    html structure change and shuffle on page load

--- script.js
+++ script.js
@@ -11,4 +11,26 @@ cards.forEach(card => {
 });
 
 //shuffle cards on page load for different order
-function shuffleCards() {}
+function shuffleCards() {
+  let cardOrder = Array.from(
+    document.querySelectorAll(".card-container .card")
+  );
+
+  let currentIndex = cardOrder.length,
+    temporaryValue,
+    randomIndex;
+
+  while (0 !== currentIndex) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    temporaryValue = cardOrder[currentIndex];
+    cardOrder[currentIndex] = cardOrder[randomIndex];
+    cardOrder[randomIndex] = temporaryValue;
+  }
+
+  // Append shuffled cards to container
+  cardOrder.forEach(card => cardContainer.appendChild(card));
+}
+
+window.onload = shuffleCards;
